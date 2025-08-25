0cff7d6d43229e250d6bf7994fd45c99ac8cadce
Samuel0322M <marinsanchezs040@gmail.com>
2025-08-25 17:26:07 -0500

    se factoriza el gender box

--- lib/components/gender_selector.dart
+++ lib/components/gender_selector.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/core/app_colors.dart';
-import 'package:flutter_application/core/text_styles.dart';
+import 'package:flutter_application/widgets/gender_box.dart';
 
-class GenderSelector
-    extends StatefulWidget {
+class GenderSelector extends StatefulWidget {
   const GenderSelector({super.key});
 
   @override
-  State<GenderSelector> createState() =>
-      _GenderSelectorState();
+  State<GenderSelector> createState() => _GenderSelectorState();
 }
 
-class _GenderSelectorState
-    extends State<GenderSelector> {
+class _GenderSelectorState extends State<GenderSelector> {
   String? selectedGender;
 
   @override
@@ -20,120 +16,25 @@ class _GenderSelectorState
     return Row(
       children: [
         //hombre
-        Expanded(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedGender =
-                    "Hombre";
-              });
-            },
-            child: Padding(
-              padding:
-                  const EdgeInsets.only(
-                    left: 16,
-                    top: 16,
-                    bottom: 16,
-                    right: 8,
-                  ),
-              child: Container(
-                decoration: BoxDecoration(
-                  color:
-                      selectedGender ==
-                          "Hombre"
-                      ? AppColors
-                            .backgroundComponentSelected
-                      : AppColors
-                            .backgroundComponent,
-                  borderRadius:
-                      BorderRadius.circular(
-                        16,
-                      ),
-                ),
-                child: Padding(
-                  padding:
-                      EdgeInsets.all(
-                        12,
-                      ),
-                  child: Column(
-                    children: [
-                      //Image.network("https://raw.githubusercontent.com/ArisGuimera/Flutter-Expert/refs/heads/master/imc_calculator/assets/images/male.png", height: 100,),
-                      Image.asset(
-                        "assets/images/male.png",
-                        height: 100,
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "Hombre",
-                        style: TextStyles
-                            .bodytext,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
+        GenderBox(
+          imagePath: "assets/images/male.png",
+          texto: "Hombre",
+          isSelected: selectedGender == "H",
+          onTap: () {
+            setState(() {
+              selectedGender = "H";
+            });
+          }),
         //mujer
-        Expanded(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedGender =
-                    "Mujer";
-              });
-            },
-            child: Padding(
-              padding:
-                  const EdgeInsets.only(
-                    left: 9,
-                    top: 16,
-                    bottom: 16,
-                    right: 18,
-                  ),
-              child: Container(
-                decoration: BoxDecoration(
-                  color:
-                      selectedGender ==
-                          "Mujer"
-                      ? AppColors
-                            .backgroundComponentSelected
-                      : AppColors
-                            .backgroundComponent,
-                  borderRadius:
-                      BorderRadius.circular(
-                        16,
-                      ),
-                ),
-                child: Padding(
-                  padding:
-                      EdgeInsets.all(
-                        12,
-                      ),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        "assets/images/female.png",
-                        height: 100,
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "Mujer",
-                        style: TextStyles
-                            .bodytext,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
+        GenderBox(
+          imagePath: "assets/images/female.png",
+          texto: "Mujer",
+          isSelected: selectedGender == "M",
+          onTap: () {
+            setState(() {
+              selectedGender = "M";
+            });
+          }),
       ],
     );
   }
---
+++ lib/widgets/gender_box.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/core/app_colors.dart';
+
+class GenderBox extends StatelessWidget {
+  final String imagePath;
+  final String texto;
+  final bool isSelected;
+  final VoidCallback? onTap;
+
+  const GenderBox({
+    super.key,
+    required this.imagePath,
+    required this.texto,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+    @override
+    Widget build(BuildContext context) {
+      return Expanded(
+        child: GestureDetector(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, top: 16, bottom: 16, right: 8),
+            child: Container(
+              decoration: BoxDecoration(
+                color: isSelected
+                    ? AppColors.backgroundComponentSelected
+                    : AppColors.backgroundComponent,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: EdgeInsetsGeometry.all(12),
+                child: Column(
+                  children: [
+                    Image.asset(imagePath, height: 100),
+                    SizedBox(height: 12),
+                    Text(texto, style: TextStyle(fontSize: 16, color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
